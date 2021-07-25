4c738a263d9c637a1a0389968e1e71ba99d0fd4e
Sonja Strieder <sonja.strieder@indigoslate.com>
2021-07-25 11:37:08 +0200

    Set up linting and formatting

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ["plugin:vue/vue3-recommended", "eslint:recommended", "@vue/prettier"],
+  plugins: ["vue"],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+};
