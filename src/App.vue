826782cc0dbe16ebccbfa2bf7055352f43c8d0ee
Sonja Strieder <sonja.strieder@indigoslate.com>
2021-07-27 21:14:12 +0200

    Set up core styles and add Bootstrap abstracts

--- src/App.vue
+++ src/App.vue
@@ -24,12 +24,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: $c-base;
-}
-</style>
