8a4b162b11c084e0b2a98bb5c6a3894ae390732c
Sonja Strieder <sonja.strieder@indigoslate.com>
2021-08-01 15:03:25 +0200

    Add more topping options

--- src/data/toppings.js
+++ src/data/toppings.js
@@ -19,4 +19,53 @@ export const broccoli = {
   image: "pizza/broccoli.svg",
 };
 
-export default [mushrooms, carrot, broccoli];
+export const redOnion = {
+  id: "ad5d90ca-422e-4e59-8199-dd114e7e5b81",
+  name: "Red onion",
+  price: 1.25,
+  image: "pizza/red-onion.svg",
+};
+
+export const pepper = {
+  id: "8bc664eb-1cbc-46cd-996e-cc43d0f1645f",
+  name: "Pepper",
+  price: 1.25,
+  image: "pizza/pepper.svg",
+};
+
+export const aubergine = {
+  id: "6e21d23e-269d-4be1-a22d-f7c3d1fe0e98",
+  name: "Aubergine",
+  price: 1.5,
+  image: "pizza/aubergine.svg",
+};
+
+export const avocado = {
+  id: "03d989a0-c79e-469b-8057-80fe59e838f1",
+  name: "Avocado",
+  price: 2,
+  image: "pizza/avocado.svg",
+};
+
+export const garanzoBeans = {
+  id: "777a58a9-0d82-415b-b256-221bf6549c9b",
+  name: "Garbanzo beans",
+  price: 1.25,
+  image: "pizza/garbanzo-beans.svg",
+};
+
+export const pinapple = {
+  id: "50bffa0d-ebd6-4832-b491-d9ada62582a1",
+  name: "Pinapple",
+  price: 1.25,
+  image: "pizza/pinapple.svg",
+};
+
+export const spinach = {
+  id: "44f9fc0c-fd01-4bf9-98ce-71a0a9f2925e",
+  name: "Spinach",
+  price: 1.25,
+  image: "pizza/spinach.svg",
+};
+
+export default [mushrooms, carrot, broccoli, redOnion, pepper, aubergine, avocado, garanzoBeans, pinapple, spinach];
